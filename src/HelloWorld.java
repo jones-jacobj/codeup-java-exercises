14ff93eb9640ac2b1f2c9584693f1a85fc9be7c3
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-07 13:56:33 -0600

    Working on Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args){
+       int myFavoriteNumber = 63;
+       String myString = "Jake";
+       //float myNumber = 3.14;
+       int x = 5;
+       System.out.println(++x);
+       System.out.println(x);
+    }    
+}
