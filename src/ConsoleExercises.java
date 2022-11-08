6287d0859320ae8c30afdc0b87bfb45f012ccafa
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-08 15:42:31 -0600

    working

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // double pi = 3.14159;
+        // System.out.printf("The value of pi is approximately %.2f", pi);
+        // System.out.println("Please input an integer");
+        // int input1 = input.nextInt();
+        // System.out.printf("You input %d", input1);
+        String[] words = new String[3];
+        System.out.println("Please enter three words seperated by spaces.");
+        String inputString = input.next();
+    }
+}
