8a7baab152199b3c45689639ec31f9c58576b9bf
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-08 15:26:27 -0600

    added ignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
-       int myFavoriteNumber = 63;
-       String myString = "Jake";
+    //    int myFavoriteNumber = 63;
+    //    String myString = "Jake";
        //float myNumber = 3.14;
        int x = 5;
        System.out.println(++x);
