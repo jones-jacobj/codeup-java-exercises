8440c8c57e905404fdcc044221e27954e470ce0a
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-09 13:40:36 -0600

    Area, and perimeter exercsie

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -8,11 +8,17 @@ public class ConsoleExercises {
         // System.out.println("Please input an integer");
         // int input1 = input.nextInt();
         // System.out.printf("You input %d", input1);
-        String[] words = new String[3];
-        System.out.println("Please enter three words seperated by spaces.");
-        String inputString = input.next();
-        String inputString2 = input.next();
-        String inputString3 = input.next();
-        System.out.printf("The words you entered were %s then %s then %s",inputString,inputString2,inputString3);
+        // System.out.println("Please enter three words seperated by spaces.");
+        // String inputString = input.next();
+        // String inputString2 = input.next();
+        // String inputString3 = input.next();
+        // System.out.printf("The words you entered were %s then %s then %s",inputString,inputString2,inputString3);
+        // String inputString = input.nextLine();
+        // System.out.printf("The words you entered were %s",inputString);
+        System.out.println("Please enter the length of the room you are in:");
+        int inputLength = input.nextInt();
+        System.out.println("Please enter the width of the room you are in:");
+        int inputWidth = input.nextInt();
+        System.out.printf("The perimiter of that room is %d and the area is %d",((inputLength*2)+(inputWidth*2)),(inputLength*inputWidth));
     }
 }
