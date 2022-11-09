caf62ae4c20a5687f1902069f7c3dd06171b539b
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-09 14:21:04 -0600

    Working on powered/ cubed

--- ControlFlowExercises.java
+++ ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         // int i = 5;
@@ -23,18 +25,34 @@ public class ControlFlowExercises {
         // }
 
         // FizzBuzz
-        for(int i=0; i<=100; i++){
-            if (i%3==0 && i%5==0){
-                System.out.println("FizzBuzz");
+        // for(int i=0; i<=100; i++){
+        //     if (i%3==0 && i%5==0){
+        //         System.out.println("FizzBuzz");
+        //     }
+        //     else if(i%3==0){
+        //         System.out.println("Fizz");
+        //     }
+        //     else if(i%5==0){
+        //         System.out.println("Fizz");
+        //     }
+        //     else{
+        //         System.out.println(i);
+        //     }
+        // }
+
+        boolean cont = true;
+        Scanner input = new Scanner(System.in);
+        while(cont == true){
+            System.out.println("Please enter a whole number");
+            int in1 = input.nextInt();
+            System.out.println("Number | Squared | Cubed");
+            for (int i=1; i<=in1; i++){
+                System.out.printf("%5d | %5d | %5d%n",i,(int)Math.pow(i,2),(int)Math.pow(i,3));
             }
-            else if(i%3==0){
-                System.out.println("Fizz");
-            }
-            else if(i%5==0){
-                System.out.println("Fizz");
-            }
-            else{
-                System.out.println(i);
+            System.out.println("Would you like to enter another number? [y,n]");
+            String response = input.next();
+            if (response == "n"){
+                cont = false;
             }
         }
     }
