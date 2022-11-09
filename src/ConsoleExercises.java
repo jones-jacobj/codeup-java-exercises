54a8967103d1f72cf5dbfc2e6699221bd0db3cfa
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-09 13:35:19 -0600

    Finished three word strings

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -11,5 +11,8 @@ public class ConsoleExercises {
         String[] words = new String[3];
         System.out.println("Please enter three words seperated by spaces.");
         String inputString = input.next();
+        String inputString2 = input.next();
+        String inputString3 = input.next();
+        System.out.printf("The words you entered were %s then %s then %s",inputString,inputString2,inputString3);
     }
 }
