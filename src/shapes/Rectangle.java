f1bb3e1b8ef5edac54be2446218c78a2d6195745
J.J. Jones <jacob.j.jones01@gmail.com>
2022-11-16 11:13:03 -0600

    Did inheritence exercises

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Rectangle {
+    protected int length, width;
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea(){
+        return this.length * this.width;
+    }
+
+    public int getPerimeter(){
+        return (2 * this.length + 2 * this.width);
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Square extends Rectangle{
+    protected int side;
+    public Square(int side){
+        super(side, side);
+        this.side = side;
+    }
+
+    public int getArea(){
+        return this.side * 2;
+    }
+
+    public int getPerimeter(){
+        return this.side * this.side;
+    }
+}
