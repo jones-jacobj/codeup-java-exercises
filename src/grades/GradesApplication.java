367e9d1415b56590f7ed38f22915060d4367d51a
J.J. Jones <jacob.j.jones01@gmail.com>
2022-11-29 09:21:17 -0600

    Finished HashMap exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -10,16 +10,34 @@ public class GradesApplication {
         Scanner s1 = new Scanner(System.in);
         HashMap<String, Student> students = new HashMap<>();
         students.put("zgulde",new Student("Zach"));
-        students.get("zgulde").addGrade2(95).addGrade2(86).addGrade2(75).addGrade2(92);
+        students.get("zgulde").addGrade2(92).addGrade2(86).addGrade2(75);
+        students.put("ryanorsinger",new Student("Ryan"));
+        students.get("ryanorsinger").addGrade2(93).addGrade2(84).addGrade2(81);
+        students.put("jreich5",new Student("Jared"));
+        students.get("jreich5").addGrade2(95).addGrade2(91).addGrade2(85);
+        students.put("fmendozaro",new Student("Fred"));
+        students.get("fmendozaro").addGrade2(91).addGrade2(81).addGrade2(79);
+        students.put("MontealegreLuis",new Student("Luis"));
+        students.get("MontealegreLuis").addGrade2(87).addGrade2(88).addGrade2(73);
         System.out.println("Welcome!");
         System.out.println("Here are the GitHub usernames of our students:");
         System.out.println("|zgulde| |ryanorsinger| |jreich5| |fmendozaro| |MontealegreLuis|");
         System.out.println("What student would you like to see more information on?");
         while(running){
-            System.out.println(students.get("zgulde").getGradeAverage());
             String input = s1.next();
-            running = false;
-            System.out.println(String.format("Name: %s - GitHub Username: %s",students.get(input),students.get(input)));
+            if (students.containsKey(input)){
+                System.out.printf("Name: %s - GitHub Username: %s%n",students.get(input).getName(),input);
+                System.out.printf("Current Average %.2f \n",students.get(input).getGradeAverage());
+            }else{
+                System.out.printf("No student named %s was found",input);
+            }
+            System.out.println("Would you like to search for another student?");
+            input = s1.next();
+            if (input.toLowerCase().equals("y") || input.toLowerCase().equals("yes")){
+                continue;
+            }else {
+                running = false;
+            }
         }
     }
 }
